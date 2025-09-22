b82bc7180aa3b9f3b7c3f6a475620df121078c41
ChrisVega <christopher.vega@intelica.com>
2025-09-22 16:42:31 -0500

    mejoras

--- src/app/components/category-sidebar/category-sidebar.ts
+++ src/app/components/category-sidebar/category-sidebar.ts
@@ -1,53 +1,53 @@
-import { Component, computed, EventEmitter, OnInit, Output, signal } from '@angular/core';
-import { Category } from '../../interfaces/category.interface';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { Component, computed, EventEmitter, OnInit, Output, signal } from "@angular/core";
+import { Category } from "../../interfaces/category.interface";
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 import { CategoryItem } from "../category-item/category-item";
-import { CATEGORIES } from '../../data/categories-data';
+import { CATEGORIES } from "../../data/categories-data";
 
 @Component({
-  selector: 'app-category-sidebar',
-  imports: [FormsModule, CommonModule, CategoryItem],
-  templateUrl: './category-sidebar.html',
-  styleUrl: './category-sidebar.css'
+	selector: "app-category-sidebar",
+	imports: [FormsModule, CommonModule, CategoryItem],
+	templateUrl: "./category-sidebar.html",
+	styleUrl: "./category-sidebar.css",
 })
 export class CategorySidebar {
- @Output() categorySelected = new EventEmitter<Category>();
+	@Output() categorySelected = new EventEmitter<Category>();
 
-  private all = signal<Category[]>(structuredClone(CATEGORIES));
+	private all = signal<Category[]>(structuredClone(CATEGORIES));
 
-  query = signal<string>('');
+	query = signal<string>("");
 
-  filtered = computed<Category[]>(() => {
-    const q = this.query().trim().toLowerCase();
-    if (!q) return this.all();
+	filtered = computed<Category[]>(() => {
+		const queryFormatted = this.query().trim().toLowerCase();
+		if (!queryFormatted) return this.all();
 
-    const filterNode = (node: Category): Category | null => {
-      const name = node.name?.toLowerCase() ?? '';
-      const selfHit = name.includes(q);
+		const filterNode = (node: Category): Category | null => {
+			const name = node.name?.toLowerCase() ?? "";
+			const selfHit = name.includes(queryFormatted);
 
-      const filteredChildren = (node.children ?? [])
-        .map(filterNode)
-        .filter(Boolean) as Category[];
+			const filteredChildren = (node.children ?? []).map(filterNode).filter(Boolean) as Category[];
 
-      if (selfHit || filteredChildren.length > 0) {
-        return {
-          ...node,
-          expanded: true,
-          children: filteredChildren
-        };
-      }
-      return null;
-    };
+			if (selfHit || filteredChildren.length > 0) {
+				return {
+					...node,
+					expanded: true,
+					children: filteredChildren,
+				};
+			}
+			return null;
+		};
 
-    return this.all().map(filterNode).filter(Boolean) as Category[];
-  });
+		return this.all().map(filterNode).filter(Boolean) as Category[];
+	});
 
-  onInput(value: string) {
-    this.query.set((value ?? '').trim());
-  }
+	onInput(value: string) {
+		this.query.set((value ?? "").trim());
+	}
 
-  onSelect(cat: Category) {
-    this.categorySelected.emit(cat);
-  }
+	onSelect(cat: Category) {
+		this.categorySelected.emit(cat);
+	}
+
+	private loadTreeCategories() {}
 }
--- src/app/data/categories-data.ts
+++ src/app/data/categories-data.ts
@@ -1,26 +1,131 @@
-import { Category } from '../interfaces/category.interface';
+import { Category } from "../interfaces/category.interface";
 
 export const CATEGORIES: Category[] = [
-  {
-    id: 'docs',
-    name: 'Documentos',
-    icon: 'bi-file-earmark-text',
-    expanded: true,
-    children: [
-      {
-        id: 'contratos',
-        name: 'Contratos',
-        icon: 'bi-folder',
-        expanded: true,
-        children: [
-          {
-            id: 'contrato-trabajo',
-            name: 'Contratos de Trabajo',
-            icon: 'bi-file-earmark',
-            link: '/assets/projects/html/modelo1/index.html'
-          }
-        ]
-      }
-    ]
-  }
+	{
+		id: "modeloc4",
+		name: "Modelo C4",
+		icon: "bi-bi-folder",
+		expanded: false,
+		children: [
+			{
+				id: "code",
+				name: "Code",
+				icon: "bi bi-folder",
+				expanded: false,
+				children: [
+					{
+						id: "db",
+						name: "Database",
+						icon: "bi-folder",
+						expanded: false,
+						children: [
+							{
+								id: "fee",
+								name: "Fee",
+								icon: "bi-file-earmark",
+								link: "/assets/projects/html/drawio/Code/Database/Fee.html",
+							},
+							{
+								id: "landingpage",
+								name: "LandingPage",
+								icon: "bi-file-earmark",
+								link: "/assets/projects/html/drawio/Code/Database/LandingPage.html",
+							},
+						],
+					},
+					{
+						id: "common",
+						name: "Common",
+						icon: "bi-file",
+						link: "/assets/projects/html/drawio/Modelo C4/Code/Common.html",
+					},
+					{
+						id: "feeaggregate",
+						name: "Fee Agreggate",
+						icon: "bi-file",
+						link: "/assets/projects/html/drawio/Modelo C4/Code/Fee Agreggate.html",
+					},
+					{
+						id: "landingpageaggregate",
+						name: "LandingPage Agreggate",
+						icon: "bi-file",
+						link: "/assets/projects/html/drawio/Modelo C4/Code/LandingPage Agreggate.html",
+					},
+					{
+						id: "masteraggregate",
+						name: "Master Agreggate",
+						icon: "bi-file",
+						link: "/assets/projects/html/drawio/Modelo C4/Code/Master Agreggate.html",
+					},
+					{
+						id: "periodaggregate",
+						name: "Period Agreggate",
+						icon: "bi-file",
+						link: "/assets/projects/html/drawio/Modelo C4/Code/Period Agreggate.html",
+					},
+				],
+			},
+			{
+				id: "component",
+				name: "Component",
+				icon: "bi bi-folder",
+				expanded: false,
+				children: [
+					{
+						id: "fee_dashboard",
+						name: "Fee Dashboard",
+						icon: "bi-folder",
+						expanded: false,
+						children: [
+							{
+								id: "fee_frontend",
+								name: "Frontend",
+								icon: "bi-file-earmark",
+								link: "/assets/projects/html/drawio/Modelo C4/Component/Fee Dashboard/Frontend.html",
+							},
+						],
+					},
+					{
+						id: "component_landingpage",
+						name: "Landingpage",
+						icon: "bi-folder",
+						expanded: false,
+						children: [
+							{
+								id: "component_landingpage_frontend",
+								name: "Frontend",
+								icon: "bi-file-earmark",
+								link: "/assets/projects/html/drawio/Modelo C4/Component/Landing Page/Frontend.html",
+							},
+						],
+					},
+					{
+						id: "component_backend",
+						name: "Backend",
+						icon: "bi-file",
+						link: "/assets/projects/html/drawio/Modelo C4/Component/Backend.html",
+					},
+				],
+			},
+			{
+				id: "container",
+				name: "Container",
+				icon: "bi bi-file",
+				link: "/assets/projects/html/drawio/Modelo C4/Container.html",
+			},
+			{
+				id: "context",
+				name: "Context",
+				icon: "bi bi-file",
+				link: "/assets/projects/html/drawio/Modelo C4/Context.html",
+			},
+		],
+	},
+
+	{
+		id: "api_rest",
+		name: "Diseño de API REST",
+		icon: "bi bi-file",
+		link: "/assets/projects/html/markdown/Diseño de apis.html",
+	},
 ];
